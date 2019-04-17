d5921826393ac957e15c6e5de9127bbc32315b07
Pedro Alejandro Tavares Hernandez <pedro tavares@tiempodevelopment.com>
2019-04-17 13:28:25 -0500

    webpack practica

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import '../css/style.scss';
+
+const clientes = ['Cliente 1','Cliente 2','Cliente 3','Cliente 4'];
+
+let html = '';
+
+clientes.forEach(cliente => {
+    html += `
+        <li>${cliente}</li>
+    `;
+});
+
+document.querySelector('#clientes').innerHTML = html;
---
+++ src/js/nosotros.js
@@ -0,0 +1,11 @@
+import '../css/style.scss';
+
+class Cliente {
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
+}
+
+const cliente = new Cliente('Juan');
+console.log('desde nosotros');
+console.log(4+9);
